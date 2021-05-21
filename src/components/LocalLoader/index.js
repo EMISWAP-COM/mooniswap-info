d17c18b47ff5f90b8571f4a9cd55a40ae4a0398f
Nikita Bezhin <bezhin.nikita@gmail.com>
2021-05-21 21:18:05 +0300

    ES-833 Redesign 3

--- src/components/LocalLoader/index.js
+++ src/components/LocalLoader/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled, { css } from 'styled-components'
-import logo from '../../assets/logo_white_text.svg'
 
 const Loader = styled.div`
   pointer-events: none;
@@ -15,59 +14,29 @@ const Loader = styled.div`
   }
 
   ${props =>
-    props.fill && !props.height
-      ? css`
+  props.fill && !props.height
+    ? css`
           height: 100vh;
           position: fixed;
           top: 0;
           z-index: 99999;
           background: #26252C;
         `
-      : css`
+    : css`
           height: 180px;
         `}
 `
-
-const Spinner = styled.div`
-  display: flex;
-  margin: 0 auto;
-  width: 40%;
-  height: 40%;
-  position: relative;
-  background: url(${logo}) no-repeat center center;
-  background-size: contain;
-  -webkit-animation: sk-bounce 2s infinite ease-in-out;
-  animation: sk-bounce 2s infinite ease-in-out;
-  -webkit-animation-delay: -1s;
-  animation-delay: -1s;
-
-  @-webkit-keyframes sk-bounce {
-    0%,
-    100% {
-      -webkit-transform: scale(0.9);
-    }
-    50% {
-      -webkit-transform: scale(1);
-    }
-  }
-
-  @keyframes sk-bounce {
-    0%,
-    100% {
-      transform: scale(0.9);
-      -webkit-transform: scale(0.9);
-    }
-    50% {
-      transform: scale(1);
-      -webkit-transform: scale(1);
-    }
-  }
-`
-
 const LocalLoader = ({ fill }) => {
   return (
     <Loader fill={fill}>
-      <Spinner />
+        <iframe
+          style={{ width: '80%', maxWidth: '800px' }}
+          src="../preloader/preloader.html"
+          height="100%"
+          width="100%"
+          frameBorder="0"
+          title="preloader"
+        />
     </Loader>
   )
 }
